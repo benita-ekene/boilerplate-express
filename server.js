02beb11bddf7cb14f3c86ec743f93caeb57f6cf7
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-11 13:48:35 +0100

    created a Time Server

--- server.js
+++ server.js
@@ -22,12 +22,22 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+// app.get('/now', (req, res, next) => {
+//   req.time = new Date().toString();
+//   next();
+// }, (req, res) => {
+//   res.send({time: req.time});
+// }) 
+
+
 app.get('/now', (req, res, next) => {
   req.time = new Date().toString();
   next();
 }, (req, res) => {
-  res.send({time: req.time});
-}) 
+  res.setHeader('Content-Type', 'application/json'); // Set the Content-Type header
+  res.send({ time: req.time });
+});
+
 
 app.use( (req, res, next) => {
   let logger = req.method + " " + req.path + " " + " - " + " " + req.ip;
